5c70bfd34c581aa7863209f011c88a1ff8a24849
joy2256 <43444497+joy2256@users.noreply.github.com>
2021-11-18 18:58:55 +0900

    Revert "Feat#7"

--- teeting_backend/finance/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.db.models import fields
-from rest_framework import serializers
-from .models import *
-
-class AnalysisSerializer(serializers.ModelSerializer) :
-    class Meta :
-        model = Analysis
-        fields = '__all__'
