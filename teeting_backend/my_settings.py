2f6a5a0733a81d9fc3856afd5cbd16fc0f8772f8
joy <joyuuuu1@gmail.com>
2021-11-22 19:51:25 +0900

    [feat#34] MySQL 연동

---
+++ teeting_backend/my_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default' : {
+        'ENGINE' : 'django.db.backends.mysql', 
+        'NAME' : 'teenting_db', 
+        'USER' : 'root', 
+        'PASSWORD' : 'password', 
+        'HOST' : 'localhost', 
+        'PORT' : '3306',
+    }
+}
+SECRET_KEY = 'django-insecure-ekox3y18$=_o!eeh&qx47dxy&t8+(f_avmzj=too-3mt^+=ng*'
